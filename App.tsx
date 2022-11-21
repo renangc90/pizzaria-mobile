1dc445891fd98421175c7db8018c01cb1769381b
Renan Castro <52758918+renangc90@users.noreply.github.com>
2022-11-21 19:12:45 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
-
 import { AuthProvider } from './src/contexts/AuthContext'
 
 export default function App() {
